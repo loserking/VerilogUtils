442a09513c1e44845c873fdcfb898866a9f77d1e
johnjohnlys <johnjohnlys@gmail.com>
2014-06-26 00:50:27 +0800

    Merge SIPO.v and PISO.v into one file

--- ShiftReg.v
+++ ShiftReg.v
@@ -36,3 +36,37 @@ module PISO #(parameter BIT = 8, NDATA = 3, TAIL = 0)
 		end
 	end
 endmodule
+
+module SIPO #(parameter BIT = 8, NDATA = 3)
+(
+	input i_clk,
+	input i_shift,
+	input [BIT-1:0] i_data,
+	output [BIT-1:0] o_data [0:NDATA-1]
+);
+	integer i;
+
+	reg [BIT-1:0] data_buf_r [0:NDATA-1];
+	reg [BIT-1:0] data_buf_w [0:NDATA-1];
+
+	assign o_data = data_buf_r;
+
+	always@(*) begin
+		if (i_shift) begin
+			for (i = 0; i < NDATA-1; i=i+1) begin
+				data_buf_w[i] = data_buf_r[i+1];
+			end
+			data_buf_w[NDATA-1] = i_data;
+		end else begin
+			for (i = 0; i < NDATA; i=i+1) begin
+				data_buf_w[i] = data_buf_r[i];
+			end
+		end
+	end
+
+	always@(posedge i_clk) begin
+		for (i = 0; i < NDATA; i=i+1) begin
+			data_buf_r[i] <= data_buf_w[i];
+		end
+	end
+endmodule
